a004d00c4d65939cbe78b7a4f367d618be76285b
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-03-08 15:43:23 +0530

    Introduction to javascript,, variables and how to swap variables
    Introduction to javascript,, variables and how to swap variables

---
+++ 01_Variable.js
@@ -0,0 +1,13 @@
+var city; //variable declaration
+city = "Pune";//variable initialization
+console.log(city)
+console.log("My city name is:", city);
+
+console.log("What is undefined ?");
+var firstName; 
+console.log("First Name variable value before Initialization:", firstName);
+firstName="Sanehal";
+console.log("First name:", firstName);
+
+firstName = "Venuka";
+console.log("First name:", firstName);
---
+++ helloWorld.js
@@ -0,0 +1,7 @@
+// Camel Case: test.js, hello.js, helloWorld.js, myFirstProgram.js
+// Kebab: test.js, hello.js, hello-world.js, my-first-program.js
+// Snake:  test.js, hello.js, hello_world.js, my_first_program.js
+
+var city; //Variable Declaration
+city = "Pune"; //Variable initialisation
+console.log(city);
