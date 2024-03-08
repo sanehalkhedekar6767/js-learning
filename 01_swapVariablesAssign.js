3df6f40113edf43f6706fa2504cfdb7a6a352401
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-03-08 15:44:47 +0530

    Assignment on variables and swapping of variables
    Assignment on variables and swapping of variables

---
+++ 01_swapVariablesAssign.js
@@ -0,0 +1,27 @@
+console.log("===== Assignment 02 =====");
+
+console.log("===== STEP 1=====");
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("== Before Swapping ==");
+console.log("sweety: ", sweety, "cutie: ", cutie);
+var temp;
+temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("== After Swapping ==");
+console.log("sweety: ",sweety, "cutie: ", cutie);
+
+console.log("===== STEP 2=====");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("== Before Swapping ==");
+console.log("num1:", num1, "num2: ", num2, "num3: ", num3);
+var temp;
+temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("== After Swapping ==");
+console.log("num1: ", num1, "num2: ", num2, "num3: ", num3);
---
+++ 01_variableAssign.js
@@ -0,0 +1,33 @@
+console.log("====== Assignment 01 ======");
+
+console.log("====== STEP 1 ======");
+var myFullName; //variable declaration
+console.log("My full name value before initialization :", myFullName);
+myFullName = "Sanehal Khedekar"; //variable initialization
+console.log("My full name value after initialization :", myFullName);
+myFullName = "Sanehal Bharat Khedekar"; //variable updating
+console.log("My full name value after updating :", myFullName);
+
+console.log("====== STEP 2 ======");
+var myLove = "I love only JS";  //variable declaration and initialization 
+console.log("Value of variable myLove is : ", myLove);
+
+console.log("====== STEP 3 ======");
+var firstName = "Sanehal";
+console.log("First Name: ",firstName);
+var lastName = "Khedekar";
+console.log("Last Name: ",lastName);
+var age = 150;
+console.log("Age: ",age);
+var address = "abc";
+console.log("Address: ",address);
+var pinCode = 123456;
+console.log("Pincode: ",pinCode);
+var gender = "Female";
+console.log("Gender: ",gender);
+var isMarried = false;
+console.log("Is Married:",isMarried);
+
+
+
+
