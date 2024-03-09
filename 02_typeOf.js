45b80c608195c52e3a22182ab350a07cfec688ae
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-03-09 14:46:48 +0530

    Datatypes and typeof operator
    Datatypes and typeof operator

---
+++ 02_typeOf.js
@@ -0,0 +1,55 @@
+console.log("=======Step 1=======");
+
+var bankName;
+console.log("=====Before initialization=====");
+console.log("Value of variable bankName is:", bankName);
+
+var typeofBankName = typeof bankName;
+console.log("Type of variable bankName is:", typeofBankName);
+
+bankName = "SBI Bank";
+console.log("=====After initialization=====");
+console.log("Value of variable bankName is:", bankName);
+
+typeofBankName = typeof bankName;
+console.log("Type of variable bankName is:", typeofBankName);
+
+console.log("=======Step 2=======");
+
+var marks = "90%";
+var typeofMarks = typeof marks;
+
+console.log("Type of variable marks is:", typeofMarks);
+console.log("Value of variable marks is:", marks);
+
+console.log("=======Step 3=======");
+
+var isWorking;
+console.log("=====Before initialization=====");
+console.log("Value of variable isWorking is:", isWorking);
+
+var typeofIsWorking = typeof isWorking;
+console.log("Type of variable isWorking is:", typeofIsWorking);
+
+isWorking = true;
+console.log("=====After initialization=====");
+console.log("Value of variable isWorking is:", isWorking);
+
+typeofIsWorking = typeof isWorking;
+console.log("Type of variable isWorking is:", typeofIsWorking);
+
+console.log("=======Step 4=======");
+
+var totalCount = "Hundred and Seven";
+console.log("totalCount:", totalCount);
+
+var typeofTotalCount = typeof totalCount;
+console.log("Type of variable totalCount is:", typeofTotalCount);
+
+totalCount = 107;
+console.log("totalCount:", totalCount);
+
+typeofTotalCount = typeof totalCount;
+console.log("Type of variable totalCount is:", typeofTotalCount);
+
+
