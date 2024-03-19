fee8f1da9cc7196f2b1ed6a1867c329134e74e5a
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-03-19 23:00:23 +0530

    Iterative statements - for loop, while loop and do while loop
    Iterative statements - for loop, while loop and do while loop

---
+++ 06_forLoop.js
@@ -0,0 +1,36 @@
+
+
+//WAP to print numbers from 0 to 10
+console.log(`// WAP to print numbers from 0 to 10`);
+for (let index = 0; index <= 10; index++) {
+    console.log(index);
+    
+}
+
+//WAP to print numbers from 15 to 24
+console.log(`//WAP to print numbers from 15 to 24`);
+for (let index = 15; index <= 24; index++) {
+   console.log(index);
+
+    
+}
+
+//WAP to print numbers from 10 to 5
+console.log(`//WAP to print numbers from 10 to 5`);
+for (let index = 10; index >= 5; index--) {
+console.log(index);
+}
+
+//WAP to print even numbers from 0 to 20
+console.log(`//WAP to print even numbers from 0 to 20`);
+for (let index = 0; index<=20 ; index = index+2) {
+console.log(index);
+}
+
+//WAP to print odd numbers from 15 to 29
+console.log(`//WAP to print odd numbers from 15 to 29`);
+for (let index = 15; index<=29 ; index = index+2) {
+console.log(index);
+}
+
+
---
+++ 06_stringTraversing.js
@@ -0,0 +1,19 @@
+
+
+console.log(`//WAP to print each character of the word "Javascript"`);
+var word = "Javascript";
+for (let index = 0; index < word.length; index++) {
+   var element = word.charAt(index);
+   console.log(element); 
+}
+
+console.log(`==== WAP to count the char a ===`);
+var word = "JavaScript";
+var count = 0;
+for (let index = 0; index<word.length; index++) {
+    var char = word.charAt(index);
+    if (char=='a') {
+        count = count+1;
+    }  
+}
+console.log(`Char a count is: ${count}`);; 
