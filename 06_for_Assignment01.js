352f6a5acfd3584a23f4da229e6cc98eac9afc5c
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-03-19 23:01:21 +0530

    Assignment on for loop statement
    Assignment on for loop statement

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,29 @@
+
+console.log(`STEP 1: print 5 to 15`);
+for (let index = 5; index <= 15; index++) {
+    console.log(index);
+}
+console.log(`STEP 2: print 50 to 40,decrementing by 1`);
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+console.log(`STEP 3: print first 15 odd no's`);
+for (let i = 1; i <= 30; i=i+2) {
+    console.log(i);   
+}
+console.log(`STEP 4: print first 10 even no's`);
+for (let i = 0; i < 20; i=i+2) {
+    console.log(i);   
+}
+console.log(`STEP 5: print table of 5`);
+for (let i = 5; i <= 50; i=i+5) {
+    console.log(i);   
+}
+console.log(`STEP 6: print table of 10`);
+for (let i = 10; i <= 100; i=i+10) {
+    console.log(i);   
+}
+console.log(`STEP 7: print table of 10 in reverse order`);
+for (let i = 100; i >= 10; i=i-10) {
+    console.log(i);   
+}
