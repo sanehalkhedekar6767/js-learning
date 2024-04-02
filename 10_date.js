58e0428bc1e83f1d8b813ff56059fa5bf7d81c7f
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-04-02 20:18:38 +0530

    Built in objects in javascript
    Built in objects in javascript

---
+++ 10_date.js
@@ -0,0 +1,27 @@
+
+const date = new Date(); 
+console.log(date);// built in object
+console.log(date.getDate());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.getDay());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMilliseconds());
+console.log(date.getMinutes());
+console.log(date.getMonth());
+console.log(date.getSeconds());
+console.log(`-----------`);
+
+var dt = new Date("April 7, 2025 23:3:00");
+console.log(dt);
+
+const d = new Date("2019-02-23");
+console.log(d);
+
+const dd = new Date("2003/10/03");
+console.log(dd);
+
+const ddd = new Date("20 july 2007");
+console.log(ddd);
+
