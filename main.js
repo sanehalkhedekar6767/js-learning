c1e7ee24cca77683cfdbce77914013f7da675a3b
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-04-13 21:52:34 +0530

    Module in ES6
    Module in ES6

---
+++ main.js
@@ -0,0 +1,6 @@
+import array from "./module.js";  //default import
+import { PI, show } from "./module.js";  //named import
+
+console.log(array);
+console.log(PI);
+show();
---
+++ module.js
@@ -0,0 +1,8 @@
+const array = [2, 3, 4, 9, 8];
+
+export const PI = 3.14;  //named export
+export let show = function(){
+    console.log(`Hey buddy. I am inside FE`);
+}
+
+export default array;  //default export
